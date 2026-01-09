83eae731d2a58ed83b856c6b6a41c697973f6855
Joshua Liddell <joshua.liddell21@gmail.com>
2026-01-09 13:33:35 -0700

    fundamentals setup

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,36 +1,146 @@
-use clap::Parser;
+use std::collections::HashMap;
 
-struct Cli {
-    args: Args,
-    // more data for the app here
+/// An application-specific error type
+#[derive(Debug)]
+enum AccountingError {
+    AccountNotFound(String),
+    AccountUnderFunded(String, u64),
+    AccountOverFunded(String, u64),
 }
 
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    name: String,
-
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+/// A transaction type. Transactions should be able to rebuild a ledger's state
+/// when they are applied in the same sequence to an empty state.
+#[derive(Debug)]
+pub enum Tx {
+    // Add variants for storing withdraw/deposit transactions
+    Deposit { account: String, amount: u64 },
+    Withdraw { account: String, amount: u64 },
 }
 
-// if needed can create subcommands and match them to functions etc.
+/// A type for managing accounts and their current currency balance
+#[derive(Debug)]
+struct Accounts {
+    accounts: HashMap<String, u64>,
+}
 
-impl Cli {
-    fn new() -> Self {
-        let args = Args::parse();
-        Self { args }
+impl Accounts {
+    /// Returns an empty instance of the [`Accounts`] type
+    pub fn new() -> Self {
+        Accounts {
+            accounts: Default::default(),
+        }
     }
 
-    fn run(self) {
-        for _ in 0..self.args.count {
-            println!("Hello {}!", self.args.name);
+    /// Either creates a new account and deposits the `amount` provided into the `signer` or adds the amount to the existing account.
+    /// # Errors
+    /// Attempted overflow
+    pub fn deposit(&mut self, signer: &str, amount: u64) -> Result<Tx, AccountingError> {
+        if let Some(account) = self.accounts.get_mut(signer) {
+            (*account)
+                .checked_add(amount)
+                .and_then(|r| {
+                    *account = r;
+                    Some(r)
+                })
+                .ok_or(AccountingError::AccountOverFunded(
+                    signer.to_string(),
+                    amount,
+                ))
+                // Using map() here is an easy way to only manipulate the non-error result
+                .map(|_| Tx::Deposit {
+                    account: signer.to_string(),
+                    amount,
+                })
+        } else {
+            self.accounts.insert(signer.to_string(), amount);
+            Ok(Tx::Deposit {
+                account: signer.to_string(),
+                amount,
+            })
         }
+    }
+
+    /// Withdraws the `amount` from the `signer` account.
+    /// # Errors
+    /// Attempted overflow
+    pub fn withdraw(&mut self, signer: &str, amount: u64) -> Result<Tx, AccountingError> {
+        todo!();
+
+        // verify exists, return error otherwise
+        // self.acccounts is where they are
+    }
+
+    /// Withdraws the amount from the sender account and deposits it in the recipient account.
+    ///
+    /// # Errors
+    /// The account doesn't exist
+    pub fn send(
+        &mut self,
+        sender: &str,
+        recipient: &str,
+        amount: u64,
+    ) -> Result<(Tx, Tx), AccountingError> {
+        todo!();
     }
 }
 
 fn main() {
-    let app = Cli::new(); // parse args and create app
-    app.run() // execute logic based on args
+    println!("Hello, accounting world!");
+
+    // We are using simple &str instances as keys
+    // for more sophisticated keys (e.g. hashes)
+    // the data type could remain the same
+    let bob = "bob";
+    let alice = "alice";
+    let charlie = "charlie";
+    let initial_amount = 100;
+
+    // Creates the basic ledger and a tx log container
+    let mut ledger = Accounts::new();
+    let mut tx_log = vec![];
+
+    // Deposit an amount to each account
+    for signer in &[bob, alice, charlie] {
+        let status = ledger.deposit(*signer, initial_amount);
+        println!("Depositing {} for {}: {:?}", signer, initial_amount, status);
+        // Add the resulting transaction to a list of transactions
+        // .unwrap() will crash the program if the status is an error.
+        tx_log.push(status.unwrap());
+    }
+
+    // Send currency from one account (bob) to the other (alice)
+    let send_amount = 10_u64;
+    let status = ledger.send(bob, alice, send_amount);
+    println!(
+        "Sent {} from {} to {}: {:?}",
+        send_amount, bob, alice, status
+    );
+
+    // Add both transactions to the transaction log
+    let (tx1, tx2) = status.unwrap();
+    tx_log.push(tx1);
+    tx_log.push(tx2);
+
+    // Withdraw everything from the accounts
+    let tx = ledger.withdraw(charlie, initial_amount).unwrap();
+    tx_log.push(tx);
+    let tx = ledger
+        .withdraw(alice, initial_amount + send_amount)
+        .unwrap();
+    tx_log.push(tx);
+
+    // Here we are withdrawing too much and there won't be a transaction
+    println!(
+        "Withdrawing {} from {}: {:?}",
+        initial_amount,
+        bob,
+        ledger.withdraw(bob, initial_amount)
+    );
+    // Withdrawing the expected amount results in a transaction
+    let tx = ledger.withdraw(bob, initial_amount - send_amount).unwrap();
+    tx_log.push(tx);
+
+    // {:?} prints the Debug implementation, {:#?} pretty-prints it
+    println!("Ledger empty: {:?}", ledger);
+    println!("The TX log: {:#?}", tx_log);
 }
